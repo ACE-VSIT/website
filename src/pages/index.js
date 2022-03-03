203b53f66754ed5685c8fe30a2cca796fafe5994
nparashar150 <nparashar150@gmail.com>
2022-03-04 00:45:56 +0530

    testing dev/prod

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/Layout/index"
+import Seo from "../components/SEO"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
