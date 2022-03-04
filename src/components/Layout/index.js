b31ef422d14834122b2e823ebb1b2b383b440eed
nparashar150 <nparashar150@gmail.com>
2022-03-05 00:42:24 +0530

    Add @fontsource

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,14 +2,17 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle, lightTheme } from "./themes/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
       <Navbar />
       {children}
       <Footer />
-    </>
+    </ThemeProvider>
   )
 }
 
