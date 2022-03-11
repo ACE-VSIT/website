da8f3c55d3ee7cdefe6878bfee87e5f70387e537
nparashar150 <nparashar150@gmail.com>
2022-03-11 20:52:43 +0530

    use IntersectionObserver inside useEffect reason: https://stackoverflow.com/a/59426367

--- src/hooks/useOnScreen.js
+++ src/hooks/useOnScreen.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react"
 export default function useOnScreen(ref) {
   const [isIntersecting, setIntersecting] = useState(false)
 
-  const observer = new IntersectionObserver(([entry]) =>
-    setIntersecting(entry.isIntersecting)
-  )
-
   useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    )
+    
     observer.observe(ref.current)
     // Remove the observer as soon as the component is unmounted
     return () => {
