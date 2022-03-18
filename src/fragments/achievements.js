a19a1fd9065e5396a9cfc91278ad83c925674509
nparashar150 <nparashar150@gmail.com>
2022-03-19 02:39:03 +0530

    create fragment for achievements page

---
+++ src/fragments/achievements.js
@@ -0,0 +1,42 @@
+import { graphql } from "gatsby"
+
+export const achievementsPage = graphql`
+  fragment achievementsPage on PrismicAchievements {
+    data {
+      hero_image {
+        gatsbyImageData(placeholder: BLURRED)
+      }
+      title {
+        text
+      }
+      body {
+        ... on PrismicAchievementsDataBodyAchievement {
+          items {
+            achievement_title {
+              text
+            }
+            college_name {
+              text
+            }
+            event_date
+            event_name {
+              text
+            }
+            position
+            winner_name {
+              document {
+                ... on PrismicMembers {
+                  data {
+                    member_name {
+                      text
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/achievements.js
+++ src/pages/achievements.js
@@ -1,38 +1,56 @@
-import React from 'react'
+import React from "react"
 import { graphql } from "gatsby"
+import Seo from "../components/SEO"
 import Layout from "../components/Layout/index"
-import Head from '../components/achievements/achievements-table-slice/Head'
-import Table from '../components/achievements/achievements-table-slice/Table'
-const driverData=[
+import Head from "../components/achievements/achievements-table-slice/Head"
+import Table from "../components/achievements/achievements-table-slice/Table"
+// import { getImage, GatsbyImage} from "gatsby-plugin-image"
+
+const driverData = [
   {
     number: 44,
-    name: 'Lewis Hamilton',
-    team: 'Mercedes',
-    country: 'United Kingdom',
-    dob: '07/01/1985',
-    placeOfBirth: 'Stevenage, England'
+    name: "Lewis Hamilton",
+    team: "Mercedes",
+    country: "United Kingdom",
+    dob: "07/01/1985",
+    placeOfBirth: "Stevenage, England",
   },
   {
     number: 44,
-    name: 'Lewis Hamilton',
-    team: 'Mercedes',
-    country: 'United Kingdom',
-    dob: '07/01/1985',
-    placeOfBirth: 'Stevenage, England'
-  }
+    name: "Lewis Hamilton",
+    team: "Mercedes",
+    country: "United Kingdom",
+    dob: "07/01/1985",
+    placeOfBirth: "Stevenage, England",
+  },
 ]
-const AchievementsPage = ({data}) => {
+const AchievementsPage = ({ data }) => {
   const nav = data?.prismicLayout?.data?.body
+  
+  // Data to be shown in table
+  const achievementsData = data?.prismicAchievements?.data?.body
+  
+  // const heroImage = getImage(data?.prismicAchievements?.data?.hero_image)
+  const heroTitle = data?.prismicAchievements?.data?.title?.text
+
   return (
     <>
-    <Layout navbar={nav}>
-    <Head title="Dynamic" />
-    <Table
-      tableData={driverData}
-      headingColumns={['#',"Name","Team","Country","Date of Birth","place of birth"]}
-      title="F1 driver"
-    />
-    </Layout>
+      <Layout navbar={nav}>
+        <Seo title={heroTitle} />
+        <Head title={heroTitle} />
+        <Table
+          tableData={driverData}
+          headingColumns={[
+            "#",
+            "Name",
+            "Team",
+            "Country",
+            "Date of Birth",
+            "place of birth",
+          ]}
+          // title="F1 driver"
+        />
+      </Layout>
     </>
   )
 }
@@ -42,6 +60,9 @@ export const AchievementsQuery = graphql`
   query AchievementsPage {
     prismicLayout {
       ...navbarInfo
+    }
+    prismicAchievements {
+      ...achievementsPage
     }
   }
 `
