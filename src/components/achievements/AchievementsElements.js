157db9a9d296917fb57cb0e19b60700d868d1bb3
nparashar150 <nparashar150@gmail.com>
2022-03-20 20:19:09 +0530

    Create HeroSlice for otherPages

--- src/components/achievements/AchievementsElements.js
+++ src/components/achievements/AchievementsElements.js
@@ -1,33 +1,35 @@
 import styled from "styled-components"
 export const Header = styled.header`
-padding: 50px 15px;
-background-color: ${props => props.theme.bg};
-text-align: center;
-margin-bottom: 40px;
-h1 {
-  font-size: 40px;
-  font-weight: 300;
-}
-@media (max-width: 760px) {
-	display: block; 
-  position: absolute;
-	top: -9999px;
-	left: -9999px;
-}
+  padding: 50px 15px;
+  background-color: ${props => props.theme.bg};
+  text-align: center;
+  margin-bottom: 40px;
+  h1 {
+    font-size: 40px;
+    font-weight: 300;
+  }
+  @media (max-width: 760px) {
+    display: block;
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
 `
-export const Tr =styled.tr`
-:nth-of-type(odd) { 
-  background: yellow; 
-}
-@media (max-width: 760px) {
-	display: block;
-  position: absolute;
-	top: -9999px;
-	left: -9999px; 
-  tr { border: 1px solid #green; }
-}
+export const Tr = styled.tr`
+  :nth-of-type(odd) {
+    background: yellow;
+  }
+  @media (max-width: 760px) {
+    display: block;
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+    tr {
+      border: 1px solid #green;
+    }
+  }
 `
-export const Td =styled.td`
+export const Td = styled.td`
 padding: 6px; 
 border: 1px solid #ccc; 
 text-align: left;
@@ -53,27 +55,27 @@ background-color: background-color: ${props => props.theme.bg};;
 }
 
 `
-export const TableContainer =styled.section`
-max-width: 800px;
-width: 90%;
-margin: 0 auto 40px;
-@media (max-width: 760px) {
-	display: block; 
-}
-
+export const TableContainer = styled.section`
+  max-width: 800px;
+  width: 90%;
+  padding: 5rem 0 0 0;
+  margin: auto 40px;
+  @media (max-width: 760px) {
+    display: block;
+  }
 `
 export const TableContainerTitle = styled.div`
-text-align: center;
-padding: 10px;
-h2 {
-  font-size: 24px;
-  font-weight: 300;
-}
+  text-align: center;
+  padding: 10px;
+  h2 {
+    font-size: 24px;
+    font-weight: 300;
+  }
 `
 export const TableContainerTable = styled.table`
-width: 100%; 
-border-collapse: collapse; 
-@media (max-width: 760px) {
-	display: block; 
-}
+  width: 100%;
+  border-collapse: collapse;
+  @media (max-width: 760px) {
+    display: block;
+  }
 `
---
+++ src/components/hero-slice-secondary/HeroSliceSecondary.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {
+  HeroSliceBgWrapper,
+  HeroSliceHeading,
+} from "./HeroSliceSecondaryElements"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+export default function HeroSliceSecondary({ image, heading }) {
+  return (
+    <>
+      <HeroSliceBgWrapper>
+        <GatsbyImage
+          image={image}
+          alt={heading}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </HeroSliceBgWrapper>
+      <HeroSliceHeading>{heading}</HeroSliceHeading>
+    </>
+  )
+}
