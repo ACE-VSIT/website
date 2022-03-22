5e3d001fab92bf4b4f9dd09a0a5c016d5fef60e6
nparashar150 <nparashar150@gmail.com>
2022-03-22 23:13:11 +0530

    Create Footer Info Slice

---
+++ src/components/footer-info/FooterInfo.js
@@ -0,0 +1,19 @@
+import React from "react"
+import AnimateIn from "../animations/AnimateIn"
+import { FooterWrapper, FooterSummary } from "./FooterInfoElements"
+import ButtonComponent from "../button/Button"
+
+export default function FooterInfo({ btn, showBtn, info }) {
+  console.log(btn, showBtn, info)
+
+  return (
+    <AnimateIn delay={500} duration={500}>
+      <FooterWrapper>
+        {info && <FooterSummary>{info}</FooterSummary>}
+        {showBtn && btn && (
+          <ButtonComponent to={btn} sm value={"Apply Now"} />
+        )}
+      </FooterWrapper>
+    </AnimateIn>
+  )
+}
