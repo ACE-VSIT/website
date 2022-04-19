9786beacf9fc9308e20dfd9f593cbbc895ec01f4
nparashar150 <nparashar150@gmail.com>
2022-04-19 16:06:03 +0530

    Modify AchievementsPage styles Fix failing build

--- src/components/achievements/achievements-table-slice/Table.js
+++ src/components/achievements/achievements-table-slice/Table.js
@@ -1,53 +1,63 @@
-import React from 'react'
+import React from "react"
 import PropTypes from "prop-types"
-import { Tr , Td ,TableContainer,TableContainerTitle, TableContainerTable} from '../AchievementsElements'
+import {
+  Tr,
+  Td,
+  TableContainer,
+  TableContainerTitle,
+  TableContainerTable,
+} from "../AchievementsElements"
+import AnimateIn from "../../animations/AnimateIn"
 
-const Table = ({tableData, headingColumns, title,breakOn ="medium" }) => {
-  let tableClass="table-container_table"
+const Table = ({ tableData, headingColumns, title, breakOn = "medium" }) => {
+  let tableClass = "table-container_table"
 
-  if(breakOn=== "small"){
-      tableClass += "table-container__table--break-sm"
-  }else if(breakOn=== "medium"){
+  if (breakOn === "small") {
+    tableClass += "table-container__table--break-sm"
+  } else if (breakOn === "medium") {
     tableClass += "table-container__table--break-md"
-} else if(breakOn=== "large"){
+  } else if (breakOn === "large") {
     tableClass += "table-container__table--break-lg"
-}
-const data = tableData.map(({event_date,winner_name,position,event_name,college_name})=>{
-   
-    return <tr >
-        <Td>{event_date}</Td>    
-        <Td>{winner_name.document.data.member_name.text}</Td>    
-        <Td>{position}</Td>    
-        <Td>{event_name.text}</Td>    
-        <Td>{college_name.text}</Td>    
-    </tr>
-})
-    return (
-    <TableContainer>
-        <TableContainerTitle>    
-            <h2>{title}</h2>    
+  }
+  const data = tableData.map(
+    ({ event_date, winner_name, position, event_name, college_name }) => {
+      return (
+        <tr>
+          <Td>{event_date}</Td>
+          <Td>{winner_name?.document?.data?.member_name?.text}</Td>
+          <Td>{position}</Td>
+          <Td>{event_name.text}</Td>
+          <Td>{college_name.text}</Td>
+        </tr>
+      )
+    }
+  )
+  return (
+    <AnimateIn>
+      <TableContainer>
+        <TableContainerTitle>
+          <h2>{title}</h2>
         </TableContainerTitle>
-        <TableContainerTable  >
-        <thead>
+        <TableContainerTable>
+          <thead>
             <Tr>
-                {headingColumns.map((col,index)=>(
-                    <th key={index}>{col}</th>
-                ))}
-            </Tr>   
-        </thead>    
-        <tbody>
-       {data}
-        </tbody>
+              {headingColumns.map((col, index) => (
+                <th key={index}>{col}</th>
+              ))}
+            </Tr>
+          </thead>
+          <tbody>{data}</tbody>
         </TableContainerTable>
-    </TableContainer>
+      </TableContainer>
+    </AnimateIn>
   )
 }
 
-Table.prototype ={
-    tableData:PropTypes.arrayOf(PropTypes.object).isRequired,
-    headingColumns:PropTypes.arrayOf(PropTypes.string).isRequired,
-    title:PropTypes.string.isRequired,
-    breakOn:PropTypes.oneOf(["small","medium","large"])
+Table.prototype = {
+  tableData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  headingColumns: PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string.isRequired,
+  breakOn: PropTypes.oneOf(["small", "medium", "large"]),
 }
 
 export default Table
