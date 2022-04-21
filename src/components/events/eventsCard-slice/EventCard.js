73e5ca287df4c6cf92d5a22a7c26ac89d0ed6f7e
nparashar150 <nparashar150@gmail.com>
2022-04-21 17:36:34 +0530

    Fix eventCard null redirect

--- src/components/events/eventsCard-slice/EventCard.js
+++ src/components/events/eventsCard-slice/EventCard.js
@@ -9,23 +9,25 @@ import {
 } from "./EventCardElements"
 import AnimateIn from "../../animations/AnimateIn"
 import React from "react"
-import { CalendarOutlined } from '@ant-design/icons';
-
+import { CalendarOutlined } from "@ant-design/icons"
 
 export default function EventCard({ name, img, date, description, link }) {
-  const Redirect = (link) => {
+  const Redirect = link => {
     link && window.open(link)
   }
   return (
     <>
       <AnimateIn delay={150} duration={500}>
-        <EventsCardWrapper onClick={ () => Redirect(`${link}`)}>
+        <EventsCardWrapper onClick={() => Redirect(link)}>
           {img && <ImageWrapper src={img} />}
           <EventDetailContainer>
-            {name && <EventName> {name}</EventName>}
-            {description && <EventDescription> {description} </EventDescription>}
+            {name && <EventName> {`${name}, 20${date.slice(-2)}`}</EventName>}
+            {description && (
+              <EventDescription> {description} </EventDescription>
+            )}
             <DateWrapper>
-              <CalendarOutlined />{date && <Date> {date}</Date>}
+              <CalendarOutlined />
+              {date && <Date> {date}</Date>}
             </DateWrapper>
           </EventDetailContainer>
         </EventsCardWrapper>
