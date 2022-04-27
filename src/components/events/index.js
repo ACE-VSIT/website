66bffed9ae84f20022d75a80f3000ba93dab9cec
nparashar150 <nparashar150@gmail.com>
2022-04-27 23:26:23 +0530

    Add filter on events page

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,21 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import EventCard from "./eventsCard-slice/EventCard"
+import ProjectFilter from "../projects/components/ProjectFilter"
 import { WrapperBody, Container } from "./eventsCard-slice/EventCardElements"
 
 export default function Event({ data }) {
+  const [filter, setFilter] = useState("Event")
+  const [eventItems, setEventItems] = useState([])
+  const filterSelect = ["Event", "ACE Hour"]
+
   const displayData = data?.allPrismicEventitem?.edges.sort(function (a, b) {
     return (
       parseInt(b.node.data.event_date.slice(-2)) -
       parseInt(a.node.data.event_date.slice(-2))
     )
   })
-  // console.log(displayData)
+
+  useEffect(() => {
+    setEventItems(displayData.filter(e => e.node.data.event_type === filter))
+  }, [filter, displayData])
+
   return (
     <>
       <WrapperBody>
         <Container>
+          <ProjectFilter categories={filterSelect} setState={setFilter} />
           {data &&
-            displayData?.map((e, index) => {
+            eventItems?.map((e, index) => {
               return (
                 <EventCard
                   link={e.node.data.event_link?.url}
--- src/fragments/eventItem.js
+++ src/fragments/eventItem.js
@@ -16,6 +16,7 @@ export const allEventitem = graphql`
       event_link {
         url
       }
+      event_type
     }
   }
 `
