c3981488efe45efc31cdb8a9de832bb0aef5c53b
nparashar150 <nparashar150@gmail.com>
2022-06-18 13:57:12 +0530

    Init monorepo

--- src/components/rich-text/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { PrismicRichText } from "@prismicio/react"
-
-const RichText = ({ richText }) => {
-  return richText ? <PrismicRichText field={richText} /> : null
-}
-
-export default RichText
