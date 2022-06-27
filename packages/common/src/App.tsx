bb227396218adb6ae41886ea9356ab1d910e11b2
nparashar150 <nparashar150@gmail.com>
2022-06-27 22:10:52 +0530

    update favicon in @ace/common

--- packages/common/src/App.tsx
+++ packages/common/src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App: React.FC = () => {
+  useEffect(() => {
+    window.location.replace('https://vipsace.org/')
+  }, [])
   return <div>MF-CRA</div>
 }
 
