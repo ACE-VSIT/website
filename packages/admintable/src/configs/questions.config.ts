06dce019d7c7977f289f9cadabffb3d964c24493
Naman Parashar <nparashar150@gmail.com>
2022-12-23 00:38:21 +0530

    Add option to filter user as per category (#78)

---
+++ packages/admintable/src/configs/questions.config.ts
@@ -0,0 +1,67 @@
+export const questions: any = {
+  "App Development": [
+    "basic_application_1",
+    "basic_application_2",
+    "todo_app",
+    "wallpaper_app"
+  ],
+  "Ar/Vr Development": [
+    "environment_designing",
+    "normal_game",
+    "coding_related"
+  ],
+  "Audio Visual": [
+    "audio_video",
+    "logo_reveal",
+    "ace_video"
+  ],
+  "Content Writing": [
+    "sponsorship_proposal",
+    "chat_gpt",
+    "article",
+    "how-to_blog"
+  ],
+  "Data Science": [
+    "analyzing",
+    "updating",
+    "comparing"
+  ],
+  "Digital Marketing": [
+    "seo",
+    "promotions",
+    "flower_shop_marketing",
+    "hack_vsit"
+  ],
+  "Graphics Designing": [
+    "website_design",
+    "bump",
+    "digi-sell",
+    "techno",
+    "blue_cypress",
+    "brand_kit"
+  ],
+  "IOT": [
+    "arduino",
+    "firebase",
+    "communication"
+  ],
+  "Miscellaneous": [
+    "miscellaneous"
+  ],
+  "Programming": [
+    "list_of_integers_1",
+    "list_of_integers_2",
+    "sorting",
+    "matrix"
+  ],
+  "Security": [
+    "password",
+    "not-regex",
+    "can_you_find_me?",
+  ],
+  "Web Development": [
+    "portfolio_website",
+    "tictactoe",
+    "music_player"
+  ]
+}
